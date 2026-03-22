9fa80acc416f6b0be29c69a03ec7acff3b7f378a
Sid Negi <ssnegi@yahoo.com>
2026-03-22 17:59:55 +1100

    Add files via upload

--- SQLAlchemyPandasPythonProcedureCheckStatus.py
+++ SQLAlchemyPandasPythonProcedureCheckStatus.py
@@ -9,7 +9,7 @@ database = "NorthWind"
 engine = create_engine('mssql+pyodbc://' + server + '/' + database + '?trusted_connection=yes&driver=ODBC+Driver+17+for+SQL+Server')
 #query = "SELECT concat(datepart(year,GETDATE()),'-',datepart(month,GETDATE()),'-',datepart(day,GETDATE())) curr_date;"
 query = "SELECT top 1 JobStatus from dbo.ETL_Job_Runs order by JobStatus asc;"
-#FAIL status will come before READY, SUCCESS status.
+#FAIL status will come before Ready, SUCCESS status.
 connection = engine.raw_connection();
 cursor = connection.cursor();
 df = pd.read_sql(query, engine);
